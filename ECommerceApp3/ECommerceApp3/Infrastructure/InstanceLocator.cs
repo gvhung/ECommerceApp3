b2a9c66f81b8c1fa9483b5689cda7d97aece7d40
jose.barbosa <jose.barbosa@PDCASEBH11>
2016-12-14 16:25:02 -0200

    Add project files.

---
+++ ECommerceApp3/ECommerceApp3/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceApp3.ViewModels;
+
+namespace ECommerceApp3.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+
+}
---
+++ ECommerceApp3/ECommerceApp3/ViewModels/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp3.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Propriedades
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        #endregion
+
+        #region Construtores
+        public MainViewModel()
+        {
+            LoadMenu();
+        }
+
+        #endregion
+
+        #region Metodos
+        private void LoadMenu()
+        {
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_product.png",
+                PageName = "ProductsPage",
+                Title = "Produtos"
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_customer.png",
+                PageName = "CustomersPage",
+                Title = "Clientes"
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_order.png",
+                PageName = "OrdersPage",
+                Title = "Pedidos"
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_delivery.png",
+                PageName = "DeliveriesPage",
+                Title = "Entregas"
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_snyc.png",
+                PageName = "SyncPage",
+                Title = "Sincronizar"
+
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_setup.png",
+                PageName = "SetupPage",
+                Title = "Configuracao"
+
+            });
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_logout.png",
+                PageName = "LogoutPage",
+                Title = "Sair"
+
+            });
+        }
+
+        #endregion
+
+    }
+
+}
